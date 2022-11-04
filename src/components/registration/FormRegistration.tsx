bdd31cffd59911226fd504f4f833c53036ef9dd9
safdar-azeem <safdarazeem00@gmail.com>
2022-11-05 04:45:03 +0500

    Add FormRegistration component for login and signup page

---
+++ src/components/registration/FormRegistration.tsx
@@ -0,0 +1,80 @@
+import { Box, Button, Container, Input, Text } from '@mantine/core'
+import { IFormSchema, IFormValues, useForm } from 'formash'
+import { Link } from 'react-router-dom'
+
+interface IFormRegistrationProps {
+	handleSubmit: (data: IFormValues) => void
+	heading: string
+	description: string
+	submitText: string
+	formSchema: IFormSchema[]
+	footerText: string
+	footerLinkText: string
+	footerLink: string
+}
+
+const FormRegistration = ({
+	handleSubmit,
+	formSchema,
+	heading,
+	description,
+	submitText,
+	footerText,
+	footerLinkText,
+	footerLink,
+}: IFormRegistrationProps) => {
+	const { formValues, formErrors, handleChange, doValidate } = useForm(formSchema)
+
+	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		if (doValidate()) handleSubmit(formValues)
+	}
+
+	return (
+		<Container size='xl'>
+			<Box className='flex w-full justify-between gap-x-7 items-center min-h-[80vh] md:min-h-screen mt-10 md:mt-0'>
+				<form onSubmit={onSubmit} className='w-full md:w-1/2 lg:w-1/3'>
+					<header className='mb-8 text-center md:text-left'>
+						<h1>{heading}</h1>
+						<Text>{description}</Text>
+					</header>
+					{formSchema.map((field: IFormSchema) => (
+						<Input.Wrapper
+							id={field.name}
+							key={field.name}
+							withAsterisk
+							label={field.label}
+							error={formErrors[field.name]}
+							className='mb-5'
+						>
+							<Input
+								id={field.name}
+								type={field.type}
+								name={field.name}
+								placeholder={field.placeholder}
+								value={formValues[field.name]}
+								onChange={handleChange}
+							/>
+						</Input.Wrapper>
+					))}
+					<Button type='submit' variant='filled' fullWidth className='mt-6'>
+						{submitText}
+					</Button>
+					<footer className='mt-6'>
+						<Text ta='center'>
+							{footerText}
+							<Link to={footerLink} className='ml-2 font-bold text-black'>
+								{footerLinkText}
+							</Link>
+						</Text>
+					</footer>
+				</form>
+				<Box className='w-2/4 hidden md:flex'>
+					<img src='/public/images/login-bg.svg' alt='Login' className='w-full' />
+				</Box>
+			</Box>
+		</Container>
+	)
+}
+
+export default FormRegistration
