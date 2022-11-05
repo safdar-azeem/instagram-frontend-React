fa6e062fd1d597d6a7c8749ec38a5279b6a6f741
safdar-azeem <safdarazeem00@gmail.com>
2022-11-05 17:38:40 +0500

    Remove name field from  loginSchema

--- src/mocks/login.schema.ts
+++ src/mocks/login.schema.ts
@@ -2,15 +2,6 @@ import { IFormSchema } from 'formash'
 
 export const loginSchema: IFormSchema[] = [
 	{
-		name: 'name',
-		label: 'Name',
-		type: 'text',
-		required: true,
-		placeholder: 'Enter your name',
-		error: 'Please enter your name',
-		value: '',
-	},
-	{
 		name: 'email',
 		label: 'Email',
 		type: 'email',
