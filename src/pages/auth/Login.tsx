e022dffc312f949d88313918642f269058427738
safdar-azeem <safdarazeem00@gmail.com>
2022-11-07 00:57:26 +0500

    Call login mutation on form submission

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,9 +1,36 @@
+import { ApolloError, useMutation } from '@apollo/client'
+import { Icon } from '@iconify-icon/react'
+import { showNotification } from '@mantine/notifications'
 import { IFormValues } from 'formash'
+import { useNavigate } from 'react-router-dom'
 import FormRegistration from '../../components/registration/FormRegistration'
+import { AppRoutes } from '../../constants/routes.constants'
+import LoginMutation from '../../graphql/mutations/login.mutation'
 import { loginSchema } from '../../mocks/login.schema'
+import iCookies from '../../utils/cookies.utils'
 
 const Login = () => {
-	const handleSubmit = (data: IFormValues) => {}
+	const navigate = useNavigate()
+
+	const [login, { loading }] = useMutation(LoginMutation, {
+		onCompleted: ({ login }) => {
+			iCookies.setToken(login.token)
+			showNotification({
+				message: 'Login successful',
+				icon: <Icon icon='mdi:check' />,
+			})
+			navigate(AppRoutes.HOME)
+		},
+		onError: (error: ApolloError) => {
+			showNotification({
+				color: 'red',
+				message: error.message,
+				icon: <Icon icon='mdi:close' />,
+			})
+		},
+	})
+
+	const handleSubmit = (data: IFormValues) => login({ variables: data })
 
 	return (
 		<FormRegistration
@@ -14,7 +41,8 @@ const Login = () => {
 			submitText='Login'
 			footerText='Don’t have an account?'
 			footerLinkText='Sign up'
-			footerLink='/signup'
+			footerLink={AppRoutes.REGISTER}
+			isLoading={loading}
 		/>
 	)
 }
