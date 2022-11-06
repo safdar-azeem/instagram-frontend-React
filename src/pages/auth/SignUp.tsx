635db56f95ecb3279decce4c6a5ed6d2b8143785
safdar-azeem <safdarazeem00@gmail.com>
2022-11-07 00:57:18 +0500

    Call register mutation on form submission

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,9 +1,34 @@
+import { ApolloError, useMutation } from '@apollo/client'
+import { Icon } from '@iconify-icon/react'
+import { showNotification } from '@mantine/notifications'
 import { IFormValues } from 'formash'
+import { useNavigate } from 'react-router-dom'
 import FormRegistration from '../../components/registration/FormRegistration'
+import { AppRoutes } from '../../constants/routes.constants'
+import RegisterMutation from '../../graphql/mutations/register.mutation'
 import { signUpSchema } from '../../mocks/signUp.schema'
 
 const SignUp = () => {
-	const handleSubmit = (data: IFormValues) => {}
+	const navigate = useNavigate()
+
+	const [register, { loading }] = useMutation(RegisterMutation, {
+		onCompleted: ({ register }) => {
+			showNotification({
+				message: register.message,
+				icon: <Icon icon='mdi:check' />,
+			})
+			navigate(AppRoutes.LOGIN)
+		},
+		onError: (error: ApolloError) => {
+			showNotification({
+				color: 'red',
+				message: error.message,
+				icon: <Icon icon='mdi:close' />,
+			})
+		},
+	})
+
+	const handleSubmit = (data: IFormValues) => register({ variables: data })
 
 	return (
 		<FormRegistration
@@ -14,7 +39,8 @@ const SignUp = () => {
 			submitText='Sign up'
 			footerText='Already have an account?'
 			footerLinkText='Login'
-			footerLink='/login'
+			footerLink={AppRoutes.REGISTER}
+			isLoading={loading}
 		/>
 	)
 }
