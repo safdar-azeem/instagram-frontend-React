09d8a8be604822304d4c8173b410b7c70386d46a
safdar-azeem <safdarazeem00@gmail.com>
2022-11-07 01:00:10 +0500

    Add ApolloProvider component

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,21 @@
-import { Box, Loader, MantineProvider } from '@mantine/core'
+import { ApolloProvider } from '@apollo/client'
+import { MantineProvider } from '@mantine/core'
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import FallBackLoader from './components/reusable/FallBackLoader'
+import client from './config/graphql.config'
 import './index.css'
 import theme from './theme/'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<Suspense
-			fallback={
-				<Box className='min-h-screen flex justify-center items-center w-screen'>
-					<Loader />
-				</Box>
-			}
-		>
-			<MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
-				<App />
-			</MantineProvider>
-		</Suspense>
+		<ApolloProvider client={client}>
+			<Suspense fallback={<FallBackLoader />}>
+				<MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
+					<App />
+				</MantineProvider>
+			</Suspense>
+		</ApolloProvider>
 	</React.StrictMode>
 )
