49f47adf11de7b612a1dce9f542d0a3cdc709468
safdar-azeem <safdarazeem00@gmail.com>
2022-11-09 05:37:49 +0500

    Define codegen configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+import codegen from 'vite-plugin-graphql-codegen'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+	return defineConfig({
+		plugins: [
+			react(),
+			codegen({
+				config: {
+					schema: process.env.VITE_APP_API_URL,
+					documents: 'src/**/*.ts',
+					config: {
+						preResolveTypes: true,
+					},
+					generates: {
+						'src/types/graphql.types.ts': {
+							plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
+							config: {
+								typesPrefix: 'I',
+								skipTypename: true,
+								flattenGeneratedTypesIncludeFragments: true,
+								dedupeOperationSuffix: true,
+							},
+						},
+					},
+				},
+			}),
+		],
+	})
+}
